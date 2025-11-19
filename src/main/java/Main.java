d93f75774c7bbdf52657c5d6f1d14f6f583ecac7
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-11-18 21:14:45 -0300

    Refatoração;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,10 @@ public class Main {
         // Declaraçao de uma variável do tipo Personagem;
         Personagem jogador1;
         jogador1 = new Personagem();
+        jogador1.nome = "João";
+        jogador1.ataque = 10;
+        jogador1.defesa = 10;
+        jogador1.vida = 200;
 
         System.out.println(jogador1);
     }
--- src/main/java/model/Personagem.java
+++ src/main/java/model/Personagem.java
@@ -1,19 +1,19 @@
 package model;
 
 public class Personagem {
-    String nome;
-    int vida;
-    int defesa;
-    int ataque;
-    boolean ativo;
+    public String nome;
+    public int vida;
+    public int defesa;
+    public int ataque;
+    public boolean ativo;
     @Override
     public String toString() {
         return "Personagem{" +
-                "nome='" + nome + '\'' +
-                ", vida=" + vida +
-                ", defesa=" + defesa +
-                ", ataque=" + ataque +
-                ", ativo=" + ativo +
-                '}';
+                "\n\tnome='" + nome + '\'' +
+                ", \n\tvida=" + vida +
+                ", \n\tdefesa=" + defesa +
+                ", \n\tataque=" + ataque +
+                ", \n\tativo=" + ativo +
+                "\n}";
     }
 }
